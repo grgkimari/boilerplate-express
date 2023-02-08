92cfeadb45f2d51e83489e96546eb84bfcfe80ff
George Kimari <grgkimari@gmail.com>
2023-02-08 22:06:06 +0300

    initial commit

--- myApp.js
+++ myApp.js
@@ -1,39 +1,19 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
 
+app.get('/json', (req, res) => {
+    res.json({
+        "message" : process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"
+    })
+})
 
+app.use('/public', express.static(__dirname + '/public'))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
