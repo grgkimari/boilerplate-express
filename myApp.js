8066511042c23865f813711b60467fa11b7c7a51
George Kimari <grgkimari@gmail.com>
2023-02-08 23:16:28 +0300

    request params

--- myApp.js
+++ myApp.js
@@ -2,11 +2,23 @@ let express = require('express');
 let app = express();
 require('dotenv').config()
 
-
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next()
+})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
+
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString()
+    next()
+},
+(req, res, next) => {
+    res.send({time : req.time})
+}
+)
 
 app.get('/json', (req, res) => {
     res.json({
@@ -15,5 +27,9 @@ app.get('/json', (req, res) => {
 })
 
 app.use('/public', express.static(__dirname + '/public'))
+
+app.get('/:word/echo', (req, res) => {
+    res.send({echo : req.params.word})
+})
 
 module.exports = app;
